f1063276be33e99e6c5cc0bb1d3e126832ca5921
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-08-22 10:18:34 -0500

    Configurando el sistema de enrutamiento

--- AngularRoutingApp/apm/src/app/app.module.ts
+++ AngularRoutingApp/apm/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -20,6 +21,11 @@ import { MessageModule } from './messages/message.module';
     BrowserModule,
     HttpModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
+    RouterModule.forRoot([
+        { path: 'welcome', component: WelcomeComponent },
+        { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+        { path: '**', component: PageNotFoundComponent } 
+    ]),
     ProductModule,
     UserModule,
     MessageModule
