e05ca2359bcc60588f8750b23aa2541f34b0a19b
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-08-23 08:26:03 -0500

    Trabajando con el sistema de enrutamiento

--- AngularRoutingApp/apm/src/app/home/welcome.component.ts
+++ AngularRoutingApp/apm/src/app/home/welcome.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-home',
+    //selector: 'app-home',
     templateUrl: './app/home/welcome.component.html'
 })
 export class WelcomeComponent {
