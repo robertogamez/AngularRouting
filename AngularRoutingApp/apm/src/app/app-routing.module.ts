16dc49c27ba4909cfe27a8d252c131bffb92f270
Roberto G <roberto.manuel.balderas.gamez@hotmail.com>
2018-09-19 19:45:04 -0500

    Se añade soporte para estrategia de precarga

--- AngularRoutingApp/apm/src/app/app-routing.module.ts
+++ AngularRoutingApp/apm/src/app/app-routing.module.ts
@@ -4,19 +4,26 @@ import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent  } from './page-not-found.component';
 import { AuthGuard } from './user/auth-guard.service';
+import { SelectiveStrategy } from './selective-strategy.service';
 
 @NgModule({
     imports: [
         RouterModule.forRoot([
             { path: 'welcome', component: WelcomeComponent },
             { 
-                path: 'products', 
+                path: 'products',
+                data: {
+                    preload: true
+                },
                 loadChildren: 'app/products/product.module#ProductModule',
-                canLoad: [ AuthGuard ]
+                canActivate: [ AuthGuard ]
             },
             { path: '', redirectTo: 'welcome', pathMatch: 'full' },
             { path: '**', component: PageNotFoundComponent }
-        ], { enableTracing: true })
+        ], { enableTracing: true, preloadingStrategy: SelectiveStrategy })
+    ],
+    providers: [
+        SelectiveStrategy
     ],
     exports: [RouterModule]
 })
---
+++ AngularRoutingApp/apm/src/app/selective-strategy.service.ts
@@ -0,0 +1,16 @@
+import { Injector, Injectable } from '@angular/core';
+import { Route, PreloadingStrategy, RouterLinkActive  } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+@Injectable()
+export class SelectiveStrategy implements PreloadingStrategy {
+
+    preload(route: Route, load: Function): Observable<any> {
+        if(route.data && route.data['preload']){
+            return load();
+        }
+
+        return Observable.of(null);
+    }
+}
